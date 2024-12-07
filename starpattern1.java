5f5c0125ac48f5809449bdf45e2b7ad801486c5e
janu47537 <janu47537@gmail.com>
2024-12-07 10:00:58 +0530

    Add files via upload

---
+++ starpattern1.java
@@ -0,0 +1,27 @@
+/*
+5
+*
+* *
+*   *
+*     *
+* * * * *
+*/
+mport java.util.*;
+public class starpattern1{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==n-1||j==0||i==j)
+                System.out.print("*"+" ");
+                else
+                System.out.print(" "+" ");
+            }
+            System.out.println();
+        }
+    }
+}
